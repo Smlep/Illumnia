078b20edce4bd344c5f4198a1227e100e2a04ead
Smlep <sam.courtemanche.ipod@gmail.com>
2016-02-05 22:09:23 +0100

    Unity du Projet
    Version Unity du jeu

---
+++ ILLUMNIA/Assets/Script/Camera1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Camera1 : MonoBehaviour {
+    public float speed;
+	void Start () {
+    }
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        transform.position+=(movement * speed);
+    }
+}
