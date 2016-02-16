ba3268b184cc2322405f64ad066c2830413c62f2
Duranof <cyril.etourneau@epita.fr>
2016-02-16 23:50:56 +0100

    porte avec lumières

---
+++ ILLUMNIA/Assets/Scripts/Changecolor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Changecolor : MonoBehaviour {
+    private bool ColorON;
+    public GameObject lampeoff;
+    public GameObject lampeon;
+	// Use this for initialization
+	void Start () {
+        if (lampeon.activeSelf == true && lampeoff.activeSelf == false)
+        {
+            ColorON = true;
+        }
+        else
+            ColorON = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Activate()
+    {
+        if (ColorON)
+        {
+            ColorON = false;
+            coloroff();
+        }
+        else
+        {
+            ColorON = true;
+            coloron();
+        }
+    }
+
+    void coloron()
+    {
+        lampeoff.SetActive(false);
+        lampeon.SetActive(true);
+    }
+
+    void coloroff()
+    {
+        lampeoff.SetActive(true);
+        lampeon.SetActive(false);
+    }
+}
