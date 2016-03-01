e70610216f0a1c0fc4d83554dd396fa7687db6c3
Smlep <sam.courtemanche.ipod@gmail.com>
2016-03-01 13:15:03 +0100

    le boss n'attaque qu'a partir d'une certaine distance

--- ILLUMNIA/Assets/FIGHT/BOSS1/Boss1Move.cs
+++ ILLUMNIA/Assets/FIGHT/BOSS1/Boss1Move.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Boss1Move : MonoBehaviour {
+public class Boss1Move : MonoBehaviour
+{
 
     Transform player;               // Reference to the player's position.
     PlayerHealth playerHealth;      // Reference to the player's health.
     EnemyHealth enemyHealth;        // Reference to this enemy's health.
-    NavMeshAgent nav;               // Reference to the nav mesh agent.
+    NavMeshAgent nav;
+    private Animation animation;
+    public int portéededétectiondujoueur;// Reference to the nav mesh agent.
 
     void Awake()
     {
@@ -15,22 +18,33 @@ public class Boss1Move : MonoBehaviour {
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
+        animation = GetComponent<Animation>();
     }
 
 
     void Update()
     {
-        // If the enemy and the player have health left...
-        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        // Si le monstre est pres du joueur
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) +
+            Mathf.Abs(player.transform.position.z - transform.position.z) < portéededétectiondujoueur)
         {
-            // ... set the destination of the nav mesh agent to the player.
-            nav.SetDestination(player.position);
+            // If the enemy and the player have health left...
+            if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+            {
+                // ... set the destination of the nav mesh agent to the player.
+                nav.SetDestination(player.position);
+            }
+            else
+            {
+                // ... disable the nav mesh agent.
+                nav.enabled = false;
+            }
         }
-        // Otherwise...
         else
         {
-            // ... disable the nav mesh agent.
-            nav.enabled = false;
+            animation.Play("idle");
         }
+        // Otherwise...
+       
     }
 }
