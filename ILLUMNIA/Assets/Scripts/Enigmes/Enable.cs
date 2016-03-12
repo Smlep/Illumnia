f68de761a1c2e042401aa2188a89fe17cc9f4f7a
Duranof <cyril.etourneau@epita.fr>
2016-03-12 01:07:41 +0100

    Enigme aile 2 changée

---
+++ ILLUMNIA/Assets/Scripts/Enigmes/Enable.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enable : MonoBehaviour {
+
+    public GameObject pyl;
+    private bool pylOn;
+	// Use this for initialization
+	void Start ()
+    {
+        pyl.SetActive(false);
+        if (pyl.activeSelf == true)
+        {
+            pylOn = true;
+        }
+        else
+        {
+            pylOn = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Activate()
+    {
+        StartCoroutine(MyMethod());
+        if (pylOn)
+        {
+            pyl.SetActive(false);
+            pylOn = false;
+        }
+        else
+        {
+            pyl.SetActive(true);
+            pylOn = true;
+        }
+    }
+
+    IEnumerator MyMethod()
+    {
+        Debug.Log("Before Waiting 1 seconds");
+        yield return new WaitForSeconds(1);
+        Debug.Log("After Waiting 1 Seconds");
+    }
+}
