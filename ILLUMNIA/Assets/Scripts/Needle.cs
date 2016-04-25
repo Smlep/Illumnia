de01d9fc11ffc595b8023ffbd2cf4f0fbb4f5cec
Duranof <cyril.etourneau@epita.fr>
2016-04-25 10:06:34 +0200

    parcour quasi fini

---
+++ ILLUMNIA/Assets/Scripts/Needle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Needle : MonoBehaviour {
+
+    public float timeon;
+    public float timeoff;
+    private bool plop = true;
+    Animation anim;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(ActivationRoutine());
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        while (plop)
+        {
+            yield return new WaitForSeconds(timeon);
+            anim.Play("Anim_TrapNeedle_Hide");
+            yield return new WaitForSeconds(timeoff);
+            anim.Play("Anim_TrapNeedle_Show");
+        }
+    }
+}
---
+++ ILLUMNIA/Assets/Scripts/Trapdmg.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trapdmg : MonoBehaviour {
+
+    private float timeBetweenAttacks = 1f;
+    private GameObject player;
+    PlayerHealth playerHealth;
+    public int trapdmg;
+    bool playerInRange;
+    float timer;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenAttacks && playerInRange)
+        {
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        timer = 0f;
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(trapdmg);
+        }
+    }
+}
