33469a9865d69d8c48ad1fcaa30017515af328d3
Choqs <vincent-974@hotmail.com>
2016-04-29 17:12:56 +0200

    BOSS3 FINAL

--- ILLUMNIA/Assets/FIGHT/Boss3/Boss3Attack.cs
+++ ILLUMNIA/Assets/FIGHT/Boss3/Boss3Attack.cs
@@ -4,11 +4,11 @@ using System.Collections;
 
 public class Boss3Attack : MonoBehaviour {
 
+    float HPpourcent;
     float timerMecanic = 5f;
     bool playerInRange;
     float TimerLaser = 0.1f;
-    public Slider enemyHealthSlider;
-    GameObject spawnenemy, boss, player, laser1, laser2, baril_fin, baril_start, baril_pos_1, baril_pos_2, baril_pos_3, baril_pos_4;
+    GameObject spawnenemy, boss, player, laser1, laser2, laser3, laser4, baril_pos_1_h, baril_pos_2_h, baril_pos_3_h, baril_fin, baril_start, baril_start_h, baril_pos_1, baril_pos_2, baril_pos_3, baril_pos_4;
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth, e1Health;
     Animation Anim;
@@ -17,10 +17,11 @@ public class Boss3Attack : MonoBehaviour {
     public GameObject enemy1;
     public GameObject enemy2;
     public GameObject enemy3;
+    private GameObject enemyhealthSliderObjectGameObject; // --
+    public Slider enemyHealthSlider; // --
     // Use this for initialization
     void Awake()
     {
-
         spawnenemy = GameObject.Find("boss3spawn");
         Boss3Move.lebosspeutbouger = false;
         player = GameObject.FindGameObjectWithTag("Player");
@@ -31,19 +32,34 @@ public class Boss3Attack : MonoBehaviour {
         baril_pos_4 = GameObject.Find("baril_pos_4");
         laser1 = GameObject.Find("laser1");
         laser2 = GameObject.Find("laser2");
+        laser3 = GameObject.Find("laser3");
+        laser4 = GameObject.Find("laser4");
         baril_fin = GameObject.Find("baril_fin");
         baril_start = GameObject.Find("baril_start");
+        baril_start_h = GameObject.Find("baril_start_h");
+        baril_pos_1_h = GameObject.Find("baril_pos_1_h");
+        baril_pos_2_h = GameObject.Find("baril_pos_2_h");
+        baril_pos_3_h = GameObject.Find("baril_pos_3_h");
         scriptdupersonnage = GameObject.FindGameObjectWithTag("Player").GetComponent<ScriptPersonnage>();
+        enemyhealthSliderObjectGameObject = GameObject.FindGameObjectWithTag("BossHealthSlider3");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        enemyHealthSlider = enemyhealthSliderObjectGameObject.GetComponent<Slider>(); // --
+        enemyHealthSlider.maxValue = enemyHealth.startingHealth; // --
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        enemyHealthSlider.value = enemyHealth.currentHealth;
         TimerLaser -= Time.deltaTime;
         timerMecanic -= Time.deltaTime;
         if (TimerLaser < 0)
         {
-            laser();
+            HPpourcent = 100 * enemyHealth.currentHealth / enemyHealth.startingHealth;
+            laserV();
+            if (HPpourcent < 50)
+                laserH();
             TimerLaser = 0.1f;
         }
         if (timerMecanic < 0)
@@ -85,7 +101,7 @@ public class Boss3Attack : MonoBehaviour {
     }
     
 
-    void laser()
+    void laserV()
     { 
         if (scriptdupersonnage.laser1 || scriptdupersonnage.laser2)
         {
@@ -118,5 +134,35 @@ public class Boss3Attack : MonoBehaviour {
             }
         }
     }
-    
+
+    void laserH()
+    {
+        if (scriptdupersonnage.laser1 || scriptdupersonnage.laser2)
+        {
+            playerHealth.TakeDamage((int)(0.4 * playerHealth.startingHealth));
+        }
+        laser3.transform.position = new Vector3(laser3.transform.position.x + 0.5f, laser3.transform.position.y, laser3.transform.position.z);
+        laser4.transform.position = new Vector3(laser4.transform.position.x + 0.5f, laser4.transform.position.y, laser4.transform.position.z);
+        if (laser3.transform.position.x > baril_fin.transform.position.x)
+        {
+            System.Random position_rayon_h = new System.Random();
+            int y = position_rayon_h.Next(1, 4);
+            switch (y)
+            {
+                case 1:
+                    laser3.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser3.transform.position.y, baril_pos_1_h.transform.position.z);
+                    laser4.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser4.transform.position.y, baril_pos_1_h.transform.position.z - 8);
+                    break;
+                case 2:
+                    laser3.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser3.transform.position.y, baril_pos_1_h.transform.position.z);
+                    laser4.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser4.transform.position.y, baril_pos_1_h.transform.position.z - 8);
+                    break;
+                case 3:
+                    laser3.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser3.transform.position.y, baril_pos_1_h.transform.position.z);
+                    laser4.transform.position = new Vector3(baril_pos_1_h.transform.position.x, laser4.transform.position.y, baril_pos_1_h.transform.position.z - 8);
+                    break;
+            }
+        }
+    }
+
 }
