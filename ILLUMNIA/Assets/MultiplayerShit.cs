81a5aff140c956fe8d90523eced7ef0a9f9c7d00
Duranof <cyril.etourneau@epita.fr>
2016-06-22 02:01:51 +0200

    changements scripts multi

---
+++ ILLUMNIA/Assets/MultiplayerShit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerShit : MonoBehaviour {
+
+    Camera SceneCamera;
+	// Use this for initialization
+	void Start () {
+        SceneCamera = GameObject.FindGameObjectWithTag("CamStart").GetComponent<Camera>();
+        SceneCamera.gameObject.SetActive(false);
+        SceneCamera.gameObject.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ILLUMNIA/Assets/Scripts/Multiplayersetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Multiplayersetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+    Camera SceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+                componentsToDisable[i].enabled = false;
+        }
+        else
+        {
+            SceneCamera = GameObject.FindGameObjectWithTag("CamStart").GetComponent<Camera>();
+            if (SceneCamera != null)
+                SceneCamera.gameObject.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnDisable()
+    {
+        if (SceneCamera!=null)
+        {
+            SceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
